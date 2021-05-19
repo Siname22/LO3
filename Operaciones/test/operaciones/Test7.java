c0815868cee22599652b7f41375465a0a574e950
Siname22 <rayuga91@gmail.com>
2021-05-19 19:52:58 +0200

    Archivo de Test7

---
+++ Operaciones/test/operaciones/Test7.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operaciones;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ *
+ * @author rayug
+ */
+public class Test6 {
+        @Test
+    public void test3Mayor() {
+        System.out.println("test3Mayor");
+        int a = 0;
+        int b = 0;
+        Operaciones instance = new Operaciones();
+        int expResult = 0;
+        int result = instance.mayor(a, b);
+        assertEquals(expResult, result);
+                
+    } 
+}
